0d02f99149ddffd5d11cb997852f74e9687cae9c
Friedrich Lindenberg <friedrich@pudo.org>
2012-06-11 16:24:24 +0200

    Very basic map bubble tree/map view of northwest region.

---
+++ img/functions/functions.js
@@ -0,0 +1,21 @@
+/*
+ * BubbleTree Style for COFOG taxonomy
+ *
+ */
+
+var BubbleTree = BubbleTree || {};
+BubbleTree.Styles = BubbleTree.Styles || {};
+
+BubbleTree.Styles.Cofog1 = {
+	'01': { icon: 'government.svg', color: '#C75746' },
+	'02': { icon: 'defence.svg', color: '#0AB971' },
+	'03': { icon: 'order-safety.svg', color: '#EC2406' }, 
+	'04': { icon: 'pollution.svg', color: '#790586' }, 
+	'05': { icon: 'environment.svg', color: '#2A3A03' },	
+	'06': { icon: 'manufactoring-construction.svg', color: '#D33673' },		
+	'07': { icon: 'health.svg', color: '#4E6D00' },											
+	'08': { icon: 'culture.svg', color: '#938626' },					
+	'09': { icon: 'education.svg', color: '#44104e' },			
+	'10': { icon: 'helping-others.svg', color: '#935B3B' }	
+
+};
