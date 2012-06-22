0da1db2a2e1a48aa06cfbb9f329f88fea7771dca
Gavin Baily <gavin@tracemedia>
2012-06-22 17:05:36 +0100

    Generalise Council Budget, Instructions

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -32,13 +32,7 @@ OpenSpending.BubbleChart = function (config) {
         bubbleStyles: {
             headaccount:  BubbleTree.Styles.CouncilAccount
         }
-        /*,
-        map: {
-            url: null,
-            layerName: null,
-            keyAttribute: null
-        }
-        */
+
     }, config);
 
     $('#preloader .txt').html('loading spending data');
@@ -46,29 +40,11 @@ OpenSpending.BubbleChart = function (config) {
     var $tooltip = $('<div class="tooltip">Tooltip</div>');
     $('.bubbletree').append($tooltip);
     $tooltip.hide();
-    
-    var formatAmount = BubbleTree.Utils.formatNumber;
 
-    function updateLegend(title, colors, limits, currency) {
-        /*
-        var currencyLabel = ' (' + currency + ')';
-        var $lg = $('#cm-map-legend');
-        $lg.html('');
-        $lg.append('<div class="title">Expenditure on '+title+'</div>');
-        $.each(colors, function(i,col) {
-            var row = $('<div class="row" />'),
-                lbl = formatAmount(limits[i])+'&nbsp;–&nbsp;'+formatAmount(limits[i+1])+
-                    (i===0?currencyLabel:'');
-            row.append('<div class="color" style="background:'+col+'"></div>');
-            row.append('<div class="lbl">'+lbl+'</div>');
-            $lg.append(row);
-        });
-        */
-    }
 
     var onNodeClick = function(node) {
 
-        // only top levels
+        // show table for top levels only
         if(node.level>=2) return;
 
 
@@ -92,55 +68,6 @@ OpenSpending.BubbleChart = function (config) {
 
         }
 
-
-
-        /*
-        var 
-        // create a nice colorscale based on the selected bubble color
-        hcl = chroma.hex(node.color).hcl(),         
-        colsc = new chroma.ColorScale({
-            colors: [
-                chroma.hcl(hcl[0], Math.min(1,hcl[1]), 0.95),
-                chroma.hcl(hcl[0], Math.min(1.2,hcl[1]), 0.8),
-                chroma.hcl(hcl[0], Math.min(1.4,hcl[1]), 0.65),
-                chroma.hcl(hcl[0], Math.min(1.6,hcl[1]), 0.5),
-                chroma.hcl(hcl[0], Math.min(1.8,hcl[1]), 0.35),
-                chroma.hcl(hcl[0], Math.min(2,hcl[1]), 0.2)
-            ],
-            limits: chroma.limits(node.breakdowns, 'q', 6, 'amount')
-        });
-
-
-        // update map legend
-        updateLegend(node.label, colsc.colors, colsc.classLimits, 
-                     OpenSpending.Utils.currencySymbol(node.currency));
-        
-        // apply colors to map
-        self.map.choropleth({
-            data: function(e) { 
-                return node.breakdowns[e[opts.map.keyAttribute]];
-            },
-            colors: function(d) {
-                if (d === undefined || isNaN(d.amount)) return '#ccc';
-                return colsc.getColor(d.amount);
-            }
-            //,duration: 200
-
-        });
-
-        
-        self.map.tooltips({ 
-            layer: opts.map.layerName,
-            content: function (e) {
-                var v = node.breakdowns[e];
-                if (v === undefined ) return ['', ''];
-                var famount = OpenSpending.Utils.formatAmountWithCommas(v.amount, 0, node.currency);
-                return [e, '<div class="lbl">Expenditure on '+node.label+'</div><div class="amount">'+famount+'</div>'];
-            },
-            delay: 300
-        });
-         */
-        // create tooltips
     };
     
     // init bubbletree
@@ -154,12 +81,11 @@ OpenSpending.BubbleChart = function (config) {
             //console.log(data);
             
             $('#preloader').remove();
-            //$('#cm-map').show();
 
             self.bt = new BubbleTree({
                 data: data,
                 container: '#cm-bubbletree',
-                bubbleType: 'color',
+                bubbleType: 'icon',//'color',
                 nodeClickCallback: onNodeClick,
                 firstNodeCallback: onNodeClick,
                 rootPath: 'img/functions/',
@@ -175,19 +101,5 @@ OpenSpending.BubbleChart = function (config) {
             });
         }
     });
-     
-    // init map
-    /*
-    var map = self.map = $K.map('#cm-map');
-    map.loadStyles('css/map.css', function() {
-        map.loadMap(opts.map.url, function() {
-            map.addLayer({
-                id: opts.map.layerName,
-                key: opts.map.keyAttribute
-            });
-        }); // map.loadMap(function())
-    }); // map.loadStyles(function())
-    
-    $('#cm-map').hide();
-    */
+
 };
